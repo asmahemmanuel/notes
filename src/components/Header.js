5b2f0ea50a793d22ab8028eff2280bead070b6b5
asmahemmanuel <atbhae100@example.com>
2022-05-27 13:06:54 +0000

    fixes2

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({ handleToggleDarkMode }) => {
 	return (
 		<div className='header'>
 			<h1>DEVPOD BOARD</h1>
-            <marquee><h2>PLEASE MAKE SURE YOU READ ALL NOTES HERE</h2></marquee>
+            <h2>PLEASE MAKE SURE YOU READ ALL NOTES HERE</h2>
 			<button
 				onClick={() =>
 					handleToggleDarkMode(
