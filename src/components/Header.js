e87c7cb1109daacda259bc034ba18b1318b6e4c0
asmahemmanuel <atbhae100@example.com>
2022-05-30 09:22:52 +0000

    nelly

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = ({ handleToggleDarkMode }) => {
 	return (
 		<div className='header'>
-			<h1>DEVPOD BOARD</h1>
-            <h2>PLEASE MAKE SURE YOU READ ALL NOTES HERE</h2>
+			<h1>NELLY'S NOTES</h1>
+            {/* <h2>PLEASE MAKE SURE YOU READ ALL NOTES HERE</h2> */}
 			<button
 				onClick={() =>
 					handleToggleDarkMode(
